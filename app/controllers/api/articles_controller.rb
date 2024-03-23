b817c9c6c8674809f3bf13c5746cd545a513e8f4
Stefan <alex_giggslam@hotmail.com>
2024-03-23 12:40:22 +0800

    Added user object attach in json

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -2,7 +2,7 @@ module Api
     class ArticlesController < ApplicationController
       def index
         @articles = Article.page(params[:page]).per(params[:per_page])
-        render json: { data: @articles, message: 'Articles retrieved successfully' }
+        render json: { data: @articles.as_json(include: { user: { only: [:id, :name, :email] } }), message: 'Articles retrieved successfully' }
       end
     end
   end
